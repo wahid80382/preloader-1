df2f7760f162b2f105ab7ccabdaf8f3ae2f0324d
wahid80382 <134073438+wahid80382@users.noreply.github.com>
2023-09-10 11:01:20 +0600

    bdfndfn
    dfndfnndfn

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// document.getElementById(`nav`).addEventListener("scroll",function(){
+//     document.getElementById(`nav`).classList.add()
+// })
+$(function(){
+    $(window).on(`scroll`,function(){
+let scroll_value = $(window).scrollTop();
+if (scroll_value > 50)
+        $(`#nav`).addClass(`fixed`);
+    else{
+        $(`#nav`).removeClass(`fixed`);
+    }})
+
+
+})
+
+
